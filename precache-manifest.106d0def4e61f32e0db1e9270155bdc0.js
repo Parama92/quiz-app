2c9c275b9ce934516b26cb601f04550f79808135
Parama92 <parama.kar@gmail.com>
2019-08-12 21:47:06 +0530

    Updates

---
+++ precache-manifest.106d0def4e61f32e0db1e9270155bdc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586fabd1899990cae19940afa605d25a",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "ac0d6af0baf0cb18f531",
+    "url": "/quiz-app/static/css/main.b2804f57.chunk.css"
+  },
+  {
+    "revision": "279cafa02bca13f0d6f7",
+    "url": "/quiz-app/static/js/2.583eec2a.chunk.js"
+  },
+  {
+    "revision": "ac0d6af0baf0cb18f531",
+    "url": "/quiz-app/static/js/main.187859b4.chunk.js"
+  },
+  {
+    "revision": "5c7c6020db862c0f7966",
+    "url": "/quiz-app/static/js/runtime~main.eb147d31.js"
+  }
+]);
